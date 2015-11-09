7a0d19dcd67f75d01045f48730173129ac0138c9
Colin Fraser <colin.fraser@enernoc.com>
2015-11-08 20:24:03 -0800

    updated xtreg

--- PanelR/R/Panel.R
+++ PanelR/R/Panel.R
@@ -3,8 +3,11 @@ library(multiwayvcov)
 library(lmtest)
 library(R6)
 
+
+
 Panel <- R6Class("Panel",
                  public = list(
+
                    data = NA,
                    i = NA,
                    t = NA,
@@ -15,17 +18,7 @@ Panel <- R6Class("Panel",
                    time_index = NA,
                    first_obs = NA,
                    last_obs = NA,
-                   
-                   #' Initialize a Panel
-                   #'
-                   #' @param data A data.table which forms the data.
-                   #' @param i The name of the column containing the unit of observation.
-                   #' @param t The name of the column containing the unit of time.
-                   #'
-                   #' @return A panel object
-                   #' @export
-                   #'
-                   #' @examples
+
                    initialize = function(data, i, t) {
                      self$data <- data
                      self$i <- i
@@ -35,83 +28,79 @@ Panel <- R6Class("Panel",
                      private$set_panel_attrs()
                      setkeyv(self$data, c(self$i, self$t))
                    },
-                   #' Within transform
-                   #'
-                   #' @param cols column names to apply the within transform to
-                   #'
-                   #' @return The within-transformed columns.
-                   #' @export
-                   #'
-                   #' @examples
+
                    within = function(cols) {
-                     result <- self$data[, c(self$i, self$t, cols), with=FALSE]
+                     result <- self$data[, c(self$i, self$t, cols), with = FALSE]
+                     d <- function(x) x - mean(x) # demean function
+
                      for (col in cols) {
-                       rep <- result[, get(col) - mean(get(col)), by=get(self$i)]$V1
-                       set(result, j=col, value=rep)
+                       rep <- result[, d(get(col)), by = get(self$i)]$V1
+                       set(result, j = col, value = rep)
                      }
-                     result[, cols, with=FALSE]
+                     result[, cols, with = FALSE]
                    },
+
                    print = function() {
                      print(self$data)
                    },
+
                    trim = function(start_date, end_date) {
                      # drops observations outside of [start_date, end_date]
-                     indices <- self$data[, .(tf = get(self$t) >= start_date & get(self$t) <= end_date)]
+                     indices <- self$data[, .(tf = get(self$t) >= start_date &
+                                                get(self$t) <= end_date)]
                      self$data <- self$data[indices$tf]
                      private$set_panel_attrs()
                    },
+
                    force_balance = function() {
                      # drops subjects with fewer than self$T observations
-                     self$data[, count := length(get(self$t)), by=get(self$i)]
+
+                     self$data[, count := length(get(self$t)), by = get(self$i)]
                      self$data <- self$data[count == self$T]
                      self$data[, count := NULL]
                      private$set_panel_attrs()
                    },
-                   xtreg = function(dependent, independent, type='fe', se='cluster') {
-                     if (type == 'fe') {
-                       # the fixed effects model is estimated using the within transformation with an intercept
-                       # as detailed here http://www.stata.com/support/faqs/statistics/intercept-in-fixed-effects-model/
-                       
-                       f <- as.formula(paste(dependent, '~', paste(independent, collapse=' + ')))
-                       data <- self$within(c(dependent, independent))
-                       grand_means <- private$grand_means(c(dependent, independent))
-                       for (col in 1:length(c(dependent, independent))) {
-                         v = data[, col, with=FALSE] + grand_means[col]
-                         set(data, j = col, value = v)
-                       }
-                       l <- lm(f, data=data)
-                       # l$df.residual <- l$df.residual - self$n + 1
-                     }
-                     
-                     if (se == 'cluster') {
-                       vc <- cluster.vcov(l, self$individual_index)
-                       t <- coeftest(l, vcov=vc)
-                       w <- waldtest(l, vcov=vc, test='F')
-                     }
-                     return(list(model=l, vcov=vc, t_test=t, F_test = w))
+
+                   xtreg = function(f, type = c('pooled', 'fe'),
+                                    se = c('pooled', 'cluster', 'robust'),
+                                    cluster = FALSE,
+                                    vcov_type = c('stata_robust', 'HC0', 'HC1',
+                                                  'HC2', 'HC3', 'HC4')) {
+
+                     type <- match.arg(type)
+                     se <- match.arg(se)
+                     vcov_type <- match.arg(vcov_type)
+
+                     f <- formula(f)
+                     return(PanelRegression$new(formula = f, panel = self,
+                                                regression_type = type,
+                                                std_err_type = se,
+                                                cluster = cluster,
+                                                vcov_type = vcov_type))
                    },
+
                    update_panel = function() {
                      private$set_panel_attrs()
+                   },
+
+                   to_dta = function(filename) {
+                     haven::write_dta(self$data, filename)
+                   },
+                   panel_summary = function(file="") {
+                     s <- paste(private$panel_summary_list(), collapse = '\n')
+                     cat(s, file = file)
                    }
                  ),
                  active = list(
                    balanced = function() {
-                     observations <- self$data[, length(get(self$t)) == self$T, by=get(self$i)][, V1]
+                     observations <- self$data[, length(get(self$t)) == self$T,
+                                               by = get(self$i)][, V1]
                      min(observations) == max(observations) # check if all equal
                    },
                    columns = function() {
                      names(self$data)
-                   },
-                   panel_summary = function() {
-                     details <- character()
-                     details[1] <- ifelse(self$balanced, 'Balanced panel.', 'Unbalanced panel.')
-                     details[2] <- paste('n =', self$n)
-                     details[3] <- paste('T =', self$T)
-                     details[4] <- paste('N =', self$N)
-                     details[5] <- paste('First observation:', self$first_obs)
-                     details[6] <- paste('Last observation:', self$last_obs)
-                     paste(details, collapse = '\n')
                    }
+
                  ),
                  private = list(
                    set_panel_attrs = function() {
@@ -122,10 +111,10 @@ Panel <- R6Class("Panel",
                      last_obs <- self$data[, max(get(self$t))]
                      if (!is.na(self$N) & self$N != N) {
                        warning(
-                         paste(self$N - N, 'of', self$N,
-                                     paste('(',round(100 * (1 -  N / self$N), 2),'%)', sep=''), 
-                                     'observations dropped'
-                               )
+                         paste(
+                           self$N - N, 'of', self$N, paste('(',round(
+                             100 * (1 -  N / self$N), 2),'%)', sep = ''),
+                           'observations dropped')
                        )
                      }
                      self$n <- n
@@ -133,22 +122,138 @@ Panel <- R6Class("Panel",
                      self$N <- N
                      self$first_obs <- first_obs
                      self$last_obs <- last_obs
-                     return(NULL)
+                     return(self)
                    },
-                   grand_means = function(colnames) {
-                     means <- numeric()
-                     for (col in colnames) {
-                       means <- append(x = means, values = self$data[, mean(get(col))])
-                     }
-                     setNames(object = means, nm = colnames)
-                     return(means)
+                   panel_summary_list = function() {
+                     details <- character()
+                     details[1] <- ifelse(self$balanced, 'Balanced panel.', 'Unbalanced panel.')
+                     details[2] <- paste('n =', self$n)
+                     details[3] <- paste('T =', self$T)
+                     details[4] <- paste('N =', self$N)
+                     details[5] <- paste('First observation:', self$first_obs)
+                     details[6] <- paste('Last observation:', self$last_obs)
+                     details
                    }
                  )
 )
 
+PanelRegression <- R6Class(
+  classname = 'PanelRegression',
+  public = list(
+    regression_type = NA,
+    panel = NA,
+    formula = NA,
+    std_err_type = NA,
+    vcov_type = NA,
+    model = NA,
+    vcov = NA,
+    t_test = NA,
+    initialize = function(formula, panel,
+                          regression_type = c('pooled', 'fe', 're'),
+                          std_err_type = c('pooled', 'robust'),
+                          cluster = FALSE,
+                          vcov_type = c('pooled', 'stata_robust', 'HC0', 'HC1',
+                                        'HC2', 'HC3', 'HC4')) {
+
+      regression_type <- match.arg(regression_type)
+      std_err_type <- match.arg(std_err_type)
+      vcov_type <- match.arg(vcov_type)
+
+      self$regression_type <- regression_type
+      self$panel <- panel
+      self$formula <- formula
+      self$vcov_type <- vcov_type
+      self$std_err_type <- std_err_type
+      self$xtreg(formula, panel, regression_type, std_err_type, cluster,
+                 vcov_type)
+    },
+
+    xtreg = function(f, panel, regression_type, std_err_type, cluster,
+                     vcov_type) {
+
+      f <- formula(f)
+
+      if (regression_type == 'pooled') {
+        data <- panel$data[, all.vars(f), with = FALSE]
+        l <- lm(f, data = data)
+      }
+
+      # Fixed effects
+      if (regression_type == 'fe') {
+        # the fixed effects model is estimated using the within
+        # transformation with an intercept
+        # as detailed here:
+        # http://www.stata.com/support/faqs/statistics/intercept-in-fixed-effects-model/
+
+        data <- panel$within(all.vars(f))
+        grand_means <- private$grand_means(panel, all.vars(f))
+        for (col in 1:length(all.vars(f))) {
+          v = data[, col, with = FALSE] + grand_means[col]
+          set(data, j = col, value = v)
+        }
+        l <- lm(f, data = as.data.frame(data))
+      }
+
+      # Random effects
+      if (regression_type == 're') {
+        print("Random effects not implemented yet")
+        return(FALSE)
+      }
+
+      ## Standard errors
+
+      # No clustering for pooled
+      if (std_err_type == 'pooled' & cluster != FALSE) {
+        stop('Cluster option not available for pooled standard error')
+      }
+
+      # Robust and not clustered
+
+      if (std_err_type == 'robust' & cluster == FALSE) {
+        vc <- cluster.vcov(l, 1:panel$N)
+      }
+
+      # Robust and clustered
+      if (std_err_type == 'robust' & cluster != FALSE) {
+        # If cluster == true, use individual index for clustering, otherwise
+        # use what is passed by cluster
+        cluster_var <- ifelse(cluster == TRUE, panel$individual_index,
+                              panel[, cluster, with = FALSE])
+        vc <- cluster.vcov(l, cluster_var)
+      }
+
+      # pooled
+      if (std_err_type == 'pooled') {
+        vc <- vcov(l)
+      }
+
+      self$model <- l
+      self$t_test <- coeftest(l, vc)
+      self$vcov <- vc
+    }
+  ),
+  private = list(
+    grand_means = function(panel, colnames) {
+      means <- numeric()
+      for (col in colnames) {
+        means <- append(x = means, values = panel$data[, mean(get(col), na.rm = T)])
+      }
+      setNames(object = means, nm = colnames)
+      return(means)
+    }
+  )
+)
+
+
+
+
 `[.Panel` <- function(x, ...) {
   x$data[...]
   x$update_panel()
+}
+
+summary.Panel <- function(x) {
+  summary(x$data)
 }
 
 
